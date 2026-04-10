2126b5a0e14970d8d8797caf63116fcf8804ff5b
happy <during4277@naver.com>
2026-04-10 17:29:45 +0900

    메인화면 구현

--- vue-memo/src/views/Home.vue
+++ vue-memo/src/views/Home.vue
@@ -1,7 +1,57 @@
+<template>
+  <div class="memo-list">
+    <!--  메모를 순회하며 출력  -->
+    <router-link :to="`/memos/${m.id}`" class="item" v-for="m in state.memos">
+      <div class="d-flex pt-3">
+        <div class="pb-3 mb-0 w-100">
+          <div class="d-flex justify-content-between">
+            <!--  메모 제목  -->
+            <b>{{ m.title }}</b>
+            <div>
+              <span role="button">삭제</span>
+            </div>
+          </div>
+          <!--     메모내용     -->
+          <div class="mt-2">{{ m.content }}</div>
+        </div>
+      </div>
+    </router-link>
+    <router-link to="/memos/add" class="add btn btn-light">추가하기</router-link>
+  </div>
+</template>
 <script setup>
+import {reactive} from "vue";
+import {StorageService} from "@/services/StrorageService.js";
+
+const storageService = new StorageService("myMemo");
+const state = reactive({
+  memos: []
+});
+
+(async function onCreated(){
+  state.memos = storageService.getItems();
+})();
 </script>
 
-<template>
-</template>
+<style lang="scss" scoped>
+.memo-list{
+  .item{
+    background: #f8f9fa;
+    border: 1px solid #eee;
+    display: block;
+    color: #000;
+    text-decoration: none;
+    padding: 20px 30px;
+    margin: 15px 0;
 
-<style lang="sass" scoped></style>
+    &:hover {
+      border-color: #aaa
+    }
+  }
+  .add {
+    display: block;
+    padding: 25px;
+    border: 1px solid #eee;
+  }
+}
+</style>
