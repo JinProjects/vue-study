3dd88f4e62bcc45817e06ab683d4f55065417e44
happy <during4277@naver.com>
2026-04-30 15:19:48 +0900

    gallery front header, footer, main commit

--- gallery/frontend/src/App.vue
+++ gallery/frontend/src/App.vue
@@ -1,7 +1,34 @@
 <script setup>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import {useAccountStore} from "@/stores/account.js";
+import {useRoute} from "vue-router";
+import {watch} from "vue";
 
+//계정 스토어
+const accountStore = useAccountStore();
+//라우트 객체
+const route = useRoute();
+//로그인 여부 확인
+const checkAccount = async () => {
+  //추후 구현
+}
+//커스텀 생성 훅
+(async function onCreated() {
+  await checkAccount();
+})
+//라우트 경로가 바뀔 때마다 로그인 여부를 확인
+watch(() => route.path, () => {
+  checkAccount();
+})
 </script>
 
 <template>
+  <Header />
+  <main>
+    <router-view>
 
+    </router-view>
+  </main>
+  <Footer />
 </template>
